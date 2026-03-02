213122d54fe26332eb0f305d98fdd2866f492278
qalb-e-mashroor <264660400+qalb-e-mashroor@users.noreply.github.com>
2026-03-02 22:27:37 +0400

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -139,7 +139,7 @@ function Gallery({ isActive }) {
 
   return (
     <section className="gallery">
-      <h2>📸 Our Beautiful Memories</h2>
+      <h2>📸 A little gallery of moments that remind me how lucky I am to know you</h2>
       <div className="photos">
         {photos.map((photo, index) => (
           <img
